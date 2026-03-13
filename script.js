772dce919e6426248cab7904f1ad807b928f2d67
invitacionxsth <bs.garciav.xico.2326@cecyte.edu.mx>
2026-03-12 20:20:15 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+
+const targetDate = new Date("2026-05-10T12:00:00").getTime();
+
+setInterval(()=>{
+
+const now = new Date().getTime();
+
+const distance = targetDate - now;
+
+const days = Math.floor(distance/(1000*60*60*24));
+const hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+const minutes = Math.floor((distance%(1000*60*60))/(1000*60));
+const seconds = Math.floor((distance%(1000*60))/1000);
+
+document.getElementById("days").innerHTML = days;
+document.getElementById("hours").innerHTML = hours;
+document.getElementById("minutes").innerHTML = minutes;
+document.getElementById("seconds").innerHTML = seconds;
+
+},1000);
+
+
+
+
+const faders = document.querySelectorAll(".fade");
+
+const appear = new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("visible");
+
+}
+
+});
+
+});
+
+
+faders.forEach(el=>{
+
+appear.observe(el);
+
+});
